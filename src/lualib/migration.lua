aa6f3a38eaa1112c7ae4e4d7038320f0e117e3c7
raiguard <caleb.heuer@gmail.com>
2020-03-04 18:14:40 -0700

    Migration module cleanup

--- src/lualib/migration.lua
+++ src/lualib/migration.lua
@@ -5,10 +5,13 @@
 -- object
 local self = {}
 
+-- locals
+local string_split = util.split
+
 -- returns true if v2 is newer than v1, false if otherwise
 function self.compare_versions(v1, v2)
-  local v1_split = util.split(v1, '.')
-  local v2_split = util.split(v2, '.')
+  local v1_split = string_split(v1, '.')
+  local v2_split = string_split(v2, '.')
   for i=1,#v1_split do
     if v1_split[i] < v2_split[i] then
       return true
@@ -17,8 +20,8 @@ function self.compare_versions(v1, v2)
   return false
 end
 
--- handle migrations generically
-function self.generic(old, migrations_table, ...)
+-- run migrations
+function self.run(old, migrations_table, ...)
   local migrate = false
   for v,f in pairs(migrations_table) do
     if migrate or self.compare_versions(old, v) then
@@ -35,7 +38,7 @@ function self.on_config_changed(e, migrations_table, ...)
   if changes then
     local old = changes.old_version
     if old then
-      self.generic(old, migrations_table, ...)
+      self.run(old, migrations_table, ...)
     else
       return false -- don't do generic migrations, because we just initialized
     end
