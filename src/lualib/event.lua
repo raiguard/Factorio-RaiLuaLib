b13e4ebd6868e98b9c4a991b75abe1de10237e42
raiguard <caleb.heuer@gmail.com>
2020-03-15 17:14:10 -0600

    Rewrite event module and event tests

---
+++ src/lualib/event.lua
@@ -0,0 +1,454 @@
+-- -------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- RAILUALIB EVENT MODULE
+-- Event registration, conditional event management, GUI event filtering.
+
+-- dependencies
+local util = require('__core__.lualib.util')
+
+-- locals
+local string_match = string.match
+local table_insert = table.insert
+local table_remove = table.remove
+
+-- object
+local self = {}
+
+-- -----------------------------------------------------------------------------
+-- DISPATCHING
+
+-- holds registered events for dispatch
+local events = {}
+-- holds conditional event data
+local conditional_events = {}
+
+-- GUI filter matching functions
+local gui_filter_matchers = {
+  string = function(element, filter) return string_match(element.name, filter) end,
+  number = function(element, filter) return element.index == filter end,
+  table = function(element, filter) return element == filter end
+}
+
+-- calls handler functions tied to an event
+-- all non-bootstrap events go through this function
+local function dispatch_event(e)
+  local global_data = global.__lualib.event.conditional_events
+  local id = e.name
+  -- set ID for special events
+  if e.nth_tick then
+    id = -e.nth_tick
+  end
+  if e.input_name then
+    id = e.input_name
+  end
+  -- error checkingw
+  if not events[id] then
+    error('Event is registered but has no handlers!')
+  end
+  for _,t in ipairs(events[id]) do -- for every handler registered to this event
+    local options = t.options
+    if not options.skip_validation then
+      -- check if any userdata has gone invalid since last iteration
+      for _,v in pairs(e) do
+        if type(v) == 'table' and v.__self and not v.valid then
+          return
+        end
+      end
+    end
+    -- if we are a conditional event, insert registered players
+    local name = t.conditional_name
+    if name then
+      local con_data = global_data[name]
+      if not con_data then error('Conditional event has been raised, but has no data!') end
+      e.registered_players = con_data.players
+      -- if there are GUI filters, check them
+      gui_filters = con_data.gui_filters[e.player_index]
+      if not gui_filters and table_size(con_data.gui_filters) > 0 then
+        goto continue
+      end
+    else
+      gui_filters = t.gui_filters
+    end
+    -- check GUI filters, if any
+    if gui_filters then
+      -- check GUI filters if they exist
+      local elem = e.element
+      if not elem then
+        -- there is no element to filter, so skip calling the handler
+        log('Event '..id..' has GUI filters but no GUI element, skipping!')
+        goto continue
+      end
+      local matchers = gui_filter_matchers
+      for i=1,#gui_filters do
+        local filter = gui_filters[i]
+        if matchers[type(filter)](elem, filter) then
+          goto call_handler
+        end
+      end
+      -- if we're here, none of the filters matched, so don't call the handler
+      goto continue
+    end
+    ::call_handler::
+    -- call the handler
+    t.handler(e)
+    ::continue::
+    if options.force_crc then
+      game.force_crc()
+    end
+  end
+  return
+end
+
+-- BOOTSTRAP EVENTS
+-- these events are handled specially and do not go through dispatch_event
+
+script.on_init(function()
+  global.__lualib = {event={conditional_events={}, groups={}, players={}}}
+  -- dispatch events
+  for _,t in ipairs(events.on_init) do
+    t.handler()
+  end
+end)
+
+script.on_load(function()
+  -- dispatch events
+  for _,t in ipairs(events.on_load) do
+    t.handler()
+  end
+  -- re-register conditional events
+  local registered = global.__lualib.event.conditional_events
+  for n,_ in pairs(registered) do
+    self.enable(n, nil, nil, true)
+  end
+end)
+
+script.on_configuration_changed(function(e)
+  -- dispatch events
+  for _,t in ipairs(events.on_configuration_changed) do
+    t.handler(e)
+  end
+end)
+
+-- -----------------------------------------------------------------------------
+-- REGISTRATION
+
+local bootstrap_events = {on_init=true, on_load=true, on_configuration_changed=true}
+
+-- register static (non-conditional) events
+-- used by register_conditional to insert the handler
+-- conditional name is not to be used by the modder - it is internal only!
+function self.register(id, handler, gui_filters, options, conditional_name)
+  options = options or {}
+  -- register handler
+  if type(id) ~= 'table' then id = {id} end
+  for _,n in pairs(id) do
+    -- create event registry if it doesn't exist
+    if not events[n] then
+      events[n] = {}
+    end
+    local registry = events[n]
+    -- create master handler if not already created
+    if not bootstrap_events[n] then
+      if #registry == 0 then
+        if type(n) == 'number' and n < 0 then
+          script.on_nth_tick(-n, dispatch_event)
+        else
+          script.on_event(n, dispatch_event)
+        end
+      end
+    end
+    -- make sure the handler has not already been registered
+    for i,t in ipairs(registry) do
+      if t.handler == handler then
+        -- remove handler for re-insertion at the bottom
+        if not options.suppress_logging then
+          log('Re-registering existing event \''..n..'\', moving to bottom')
+        end
+        table_remove(registry, i)
+      end
+    end
+    -- nest GUI filters into an array if they're not already
+    if gui_filters then
+      if type(gui_filters) ~= 'table' or gui_filters.gui then
+        gui_filters = {gui_filters}
+      end
+    end
+    -- insert handler
+    local data = {handler=handler, gui_filters=gui_filters, options=options, conditional_name=conditional_name}
+    if options.insert_at then
+      table_insert(registry, options.insert_at, data)
+    else
+      table_insert(registry, data)
+    end
+  end
+  return
+end
+
+-- register conditional (non-static) events
+-- called in on_init and on_load ONLY
+function self.register_conditional(data)
+  for n,t in pairs(data) do
+    if conditional_events[n] then
+      error('Duplicate conditional event: '..n)
+    end
+    t.options = t.options or {}
+    -- add to conditional events table
+    conditional_events[n] = t
+  end
+end
+
+-- enables a conditional event
+function self.enable(name, player_index, gui_filters, reregister)
+  local data = conditional_events[name]
+  if not data then
+    error('Conditional event \''..name..'\' was not registered and has no data!')
+  end
+  local global_data = global.__lualib.event
+  local saved_data = global_data.conditional_events[name]
+  local add_player_data = false
+  -- nest GUI filters into an array if they're not already
+  if gui_filters then
+    if type(gui_filters) ~= 'table' or gui_filters.gui then
+      gui_filters = {gui_filters}
+    end
+  end
+  if saved_data then
+    -- update existing data / add this player
+    if player_index then
+      if saved_data == true then error('Tried to add a player to a global conditional event!') end
+      local player_lookup = global_data.players[player_index]
+      -- check if they're already registered
+      if player_lookup and player_lookup[name] then
+        -- don't do anything
+        if not data.options.suppress_logging then
+          log('Tried to re-register conditional event \''..name..'\' for player '..player_index..', skipping!')
+        end
+        return
+      else
+        add_player_data = true
+      end
+    elseif not reregister then
+      if not data.options.suppress_logging then
+        log('Conditional event \''..name..'\' was already registered, skipping!')
+      end
+      return
+    end
+  else
+    -- add to global
+    if player_index then
+      global_data.conditional_events[name] = {gui_filters={}, players={}}
+      add_player_data = true
+    else
+      global_data.conditional_events[name] = true
+    end
+    saved_data = global_data.conditional_events[name]
+  end
+  -- add to player lookup table
+  if add_player_data then
+    local player_lookup = global_data.players[player_index]
+    -- add the player to the event
+    saved_data.gui_filters[player_index] = gui_filters
+    table_insert(saved_data.players, player_index)
+    -- add to player lookup table
+    if not player_lookup then
+      global_data.players[player_index] = {[name]=true}
+    else
+      player_lookup[name] = true
+    end
+  end
+  -- register handler
+  self.register(data.id, data.handler, data.gui_filters, data.options, name)
+end
+
+-- disables a conditional event
+function self.disable(name, player_index)
+  local data = conditional_events[name]
+  if not data then
+    log('Tried to deregister conditional event \''..name..'\', which does not exist!')
+    return
+  end
+  local global_data = global.__lualib.event
+  local saved_data = global_data.conditional_events[name]
+  if not saved_data then error('Conditional event \''..name..'\' has local data, but no global data!') end
+  -- remove player from / manipulate global data
+  if player_index then
+    -- check if the player is actually registered to this event
+    if global_data.players[player_index][name] then
+      -- remove from players subtable
+      for i,pi in ipairs(saved_data.players) do
+        if pi == player_index then
+          table.remove(saved_data.players, i)
+          break
+        end
+      end
+      -- remove GUI filters
+      saved_data.gui_filters[player_index] = nil
+      -- remove from lookup table
+      global_data.players[player_index][name] = nil
+      -- remove lookup table if it's empty
+      if #global_data.players[player_index] == 0 then
+        global_data.players[player_index] = nil
+      end
+    else
+      log('Tried to deregister conditional event \''..name..'\' from player #'..player_index..' when it wasn\'t registered to them!')
+      return
+    end
+    if #saved_data.players == 0 then
+      global_data.conditional_events[name] = nil
+    else
+      -- don't do anything else
+      return self
+    end
+  else
+    if type(saved_data) == 'table' then
+      -- remove from all player lookup tables
+      local players = global_data.players
+      for i=1,#saved_data.players do
+        players[saved_data.players[i]][name] = nil
+      end
+    end
+    global_data.conditional_events[name] = nil
+  end
+  -- deregister handler
+  local id = data.id
+  if type(id) ~= 'table' then id = {id} end
+  for _,n in pairs(id) do
+    local registry = events[n]
+    -- error checking
+    if not registry or #registry == 0 then
+      log('Tried to deregister an unregistered event of id: '..n)
+      return
+    end
+    -- remove the handler from the events tables
+    for i,t in ipairs(registry) do
+      if t.handler == data.handler then
+        table.remove(registry, i)
+      end
+    end
+    -- de-register the master handler if it's no longer needed
+    if #registry == 0 then
+      if type(n) == 'number' and n < 0 then
+        script.on_nth_tick(math.abs(n), nil)
+      else
+        script.on_event(n, nil)
+      end
+      events[n] = nil
+    end
+  end
+end
+
+-- -------------------------------------
+-- SHORTCUT FUNCTIONS
+
+-- bootstrap events
+function self.on_init(handler, options)
+  return self.register('on_init', handler, nil, options)
+end
+
+function self.on_load(handler, options)
+  return self.register('on_load', handler, nil, options)
+end
+
+function self.on_configuration_changed(handler, options)
+  return self.register('on_configuration_changed', handler, nil, options)
+end
+
+function self.on_nth_tick(nthTick, handler, options)
+  return self.register(-nthTick, handler, nil, options)
+end
+
+-- defines.events
+for n,id in pairs(defines.events) do
+  self[n] = function(handler, options)
+    self.register(id, handler, options)
+  end
+end
+
+-- -----------------------------------------------------------------------------
+-- EVENT MANIPULATION
+
+-- raises an event as if it were actually called
+function self.raise(id, table)
+  script.raise_event(id, table)
+  return self
+end
+
+-- set or remove event filters
+function self.set_filters(id, filters)
+  if type(id) ~= 'table' then id = {id} end
+  for _,n in pairs(id) do
+    script.set_event_filter(n, filters)
+  end
+  return self
+end
+
+-- holds custom event IDs
+local custom_id_registry = {}
+
+-- generates or retrieves a custom event ID
+function self.get_id(name)
+  if not custom_id_registry[name] then
+    custom_id_registry[name] = script.generate_event_name()
+  end
+  return custom_id_registry[name]
+end
+
+-- saves a custom event ID
+function self.save_id(name, id)
+  if custom_id_registry[name] then
+    log('Overwriting entry in custom event registry: '..name)
+  end
+  custom_id_registry[name] = id
+end
+
+-- appends an array with the elements in the second array
+local function append_array(t1, t2)
+  local t1_len = #t1
+  for i=1,#t2 do
+    t1[t1_len+i] = t2[i]
+  end
+  return t1
+end
+-- updates the GUI filters for the given conditional event
+function self.update_gui_filters(name, player_index, filters, append_mode)
+  if type(filters) ~= 'table' or filters.gui then
+    filters = {filters}
+  end
+  local event_data = global.__lualib.event.conditional_events[name]
+  if not event_data then error('Cannot update GUI filters for a non-existent event!') end
+  if append_mode then
+    local filters_t = event_data.gui_filters
+    filters_t[player_index] = append_array(filters_t[player_index], filters)
+  else
+    event_data.gui_filters[player_index] = filters
+  end
+end
+
+-- retrieves and returns the global data for the given conditional event
+function self.get_event_data(name)
+  return global.__lualib.event.conditional_events[name]
+end
+
+-- returns true if the conditional event is registered
+function self.is_registered(name, player_index)
+  local global_data = global.__lualib.event
+  local registry = global_data.conditional_events[name]
+  if registry then
+    if player_index then
+      for _,i in ipairs(registry.players) do
+        if i == player_index then
+          return true
+        end
+      end
+      return false
+    end
+    return true
+  end
+  return false
+end
+
+-- -----------------------------------------------------------------------------
+
+self.events = events
+self.conditional_events = conditional_events
+
+return self
---
+++ src/tests/event/control.lua
@@ -0,0 +1,220 @@
+-- -------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- TEST EVENTS
+
+local event = require('__RaiLuaLib__.lualib.event')
+local mod_gui = require('mod-gui')
+
+-- can also use event.register('on_init', function) if so desired
+event.on_init(function()
+  log('on_init')
+  global.players = {}
+  global.chests = {}
+end)
+
+-- can also use event.on_configuration_changed(function) if so desired
+event.register('on_configuration_changed', function(e)
+  print(serpent.block(e))
+end)
+
+-- use shortcuts when we only want to listen to one event that's not custom
+event.on_player_created(function(e)
+  local player = game.players[e.player_index]
+  if player.character then
+  player.character.destructible = false
+  end
+  global.players[e.player_index] = {}
+end)
+
+-- listen to multiple events by defining them in an array
+event.register({defines.events.on_built_entity, defines.events.on_robot_built_entity, defines.events.script_raised_built}, function(e)
+  local entity = e.created_entity
+  entity.surface.create_entity{
+    name = 'highlight-box',
+    position = entity.position,
+    bounding_box = entity.selection_box,
+    box_type = 'electricity',
+    render_player_index = e.player_index,
+    blink_interval = 10,
+    time_to_live = 50
+  }
+end)
+
+-- custom inputs
+event.register('demo-input', function(e)
+  game.players[e.player_index].print('You shift+scrolled upwards on tick '..e.tick)
+end)
+
+-- nth tick
+-- can also use event.register(-3600, handler)
+event.on_nth_tick(3600, function(e)
+  game.print('It has been one minute since I last spoke!')
+end)
+
+-- -----------------------------------------------------------------------------
+-- CONDITIONAL EVENTS
+
+-- -------------------------------------
+-- GLOBAL
+-- void the contents of all wooden chests on the map on every tick
+
+local function void_chests_tick(e)
+  for _,entity in pairs(global.chests) do
+    -- remove all contents from all wooden chests on every tick
+    local inventory = entity.get_inventory(defines.inventory.chest)
+    inventory.clear()
+  end
+end
+
+event.register({defines.events.on_built_entity, defines.events.on_robot_built_entity, defines.events.script_raised_built}, function(e)
+  local entity = e.created_entity
+  if entity.valid and entity.name == 'wooden-chest' then
+    if table_size(global.chests) == 0 then
+      -- enable void chest event
+      event.enable('void_chests_tick')
+    end
+    global.chests[entity.unit_number] = entity
+  end
+end)
+
+event.register(
+  {
+    defines.events.on_player_mined_entity,
+    defines.events.on_robot_mined_entity,
+    defines.events.on_entity_died,
+    defines.events.script_raised_destroy
+  },
+  function(e)
+    local entity = e.entity
+    if entity.name == 'wooden-chest' then
+      global.chests[entity.unit_number] = nil
+      if table_size(global.chests) == 0 then
+      -- disable the void chest event
+      event.disable('void_chests_tick')
+      end
+    end
+  end
+)
+
+-- -------------------------------------
+-- PLAYER SPECIFIC
+-- when the shortcut is active, place fire at the player's feet
+
+-- places fire at the player's feet
+local function place_fire(e)
+  -- use the registered_players table that is passed with conditional events
+  for _,i in pairs(e.registered_players) do
+    local player = game.players[i]
+    player.surface.create_entity{
+      name = 'fire-flame',
+      position = player.position
+    }
+  end
+end
+
+-- when the fire shortcut is pressed
+event.register(defines.events.on_lua_shortcut, function(e)
+  local player = game.players[e.player_index]
+  if player.is_shortcut_toggled('toggle-fire-at-feet') then
+    player.set_shortcut_toggled('toggle-fire-at-feet', false)
+    -- disable the conditional event for this player specifically
+    event.disable('place_fire_at_feet', e.player_index)
+  else
+    player.set_shortcut_toggled('toggle-fire-at-feet', true)
+    -- enable the conditional event for this player specifically
+    event.enable('place_fire_at_feet', e.player_index)
+  end
+end)
+
+-- -------------------------------------
+-- REGISTRATION
+-- conditional events are registered in on_init and on_load, then enabled/disabled during runtime
+
+-- register the conditional events to allow for save/load safety
+event.register({'on_init', 'on_load'}, function()
+  event.register_conditional{
+    place_fire_at_feet = {id=-6, handler=place_fire, options={skip_validation=true}},
+    void_chests_tick = {id=defines.events.on_tick, handler=void_chests_tick}
+  }
+end)
+
+-- -----------------------------------------------------------------------------
+-- GUI EVENTS
+
+-- change daytime depending on value of slider
+-- conditionally registered if the GUI is open
+local function set_daytime(e)
+  local surface = game.players[e.player_index].surface
+  surface.daytime = e.element.slider_value
+  -- update other players' sliders
+  for _,i in pairs(e.registered_players) do
+    if i ~= e.player_index then
+      global.players[i].slider.slider_value = e.element.slider_value
+    end
+  end
+end
+
+-- create some demo buttons
+event.on_player_created(function(e)
+  local player = game.players[e.player_index]
+  local button_flow = mod_gui.get_button_flow(player)
+  button_flow.add{type='button', name='reh_demo_button_1', style=mod_gui.button_style, caption='DEMO1'}
+  button_flow.add{type='button', name='reh_demo_button_2', style=mod_gui.button_style, caption='DEMO2'}
+end)
+
+-- a clicked event for both buttons
+event.on_gui_click(function(e)
+  local player = game.players[e.player_index]
+  player.print('[color=0,255,100]you clicked a demo button![/color]')
+end, 'reh_demo_button')
+
+-- clicked event for demo button 1
+event.on_gui_click(function(e)
+  local player = game.players[e.player_index]
+  player.print('[color=255,50,50]you clicked the first demo button![/color]')
+  -- raise the custom event
+  event.raise(event.get_id('custom_event'), {player_index=e.player_index})
+end, 'reh_demo_button_1')
+
+-- clicked event for demo button 2
+-- show/hide a mod GUI frame when clicked
+event.on_gui_click(function(e)
+  local player = game.players[e.player_index]
+  local frame_flow = mod_gui.get_frame_flow(player)
+  if frame_flow.reh_demo_window then
+    -- close demo GUI
+    frame_flow.reh_demo_window.destroy()
+    -- disable the slider event
+    event.disable('change_daytime_slider', e.player_index)
+    -- remove slider from global
+    global.players[e.player_index].slider = nil
+  else
+    -- create a demo GUI
+    local window = frame_flow.add{type='frame', name='reh_demo_window', style=mod_gui.frame_style, direction='vertical', caption='REH Demo'}
+    local slider = window.add{type='slider', name='reh_demo_slider', minimum_value=0, maximum_value=1, value=player.surface.daytime}
+    -- enable slider event and set its GUI filters
+    event.enable('change_daytime_slider', e.player_index, slider)
+    -- add slider to global
+    global.players[e.player_index].slider = slider
+  end
+end, 'reh_demo_button_2')
+
+-- register the conditional events to allow for save/load safety
+event.register({'on_init', 'on_load'}, function()
+  event.register_conditional{
+    change_daytime_slider = {id=defines.events.on_gui_value_changed, handler=set_daytime}
+  }
+end)
+
+-- -----------------------------------------------------------------------------
+-- CUSTOM EVENTS
+
+-- generate the event
+-- you could also assign this to a local variable if desired
+event.get_id('custom_event')
+
+-- register a handler for the custom event
+event.register(event.get_id('custom_event'), function(e)
+  game.print('[color=0,255,255]Player '..e.player_index..' fired the custom event![/color]')
+end)
+
+-- the custom event can also be used conditionally just like any other event
