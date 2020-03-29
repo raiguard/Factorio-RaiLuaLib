0210a6aaccbc0c89746af90559a2424c1eb1e030
raiguard <caleb.heuer@gmail.com>
2020-03-29 14:22:19 -0600

    Remove debug hotkey

--- src/control.lua
+++ src/control.lua
@@ -4,8 +4,3 @@ local tests = require('tests.tests')
 for _,test in pairs(tests) do
   require('tests.'..test..'.control')
 end
-
-script.on_event('debug-inspect-global', function(e)
-  local registry = event.get_registry()
-  local breakpoint -- put breakpoint here to inspect global at any time
-end)
--- src/data.lua
+++ src/data.lua
@@ -2,11 +2,3 @@ local tests = require('tests.tests')
 for _,test in pairs(tests) do
   require('tests.'..test..'.data')
 end
-
-data:extend{
-  {
-    type = 'custom-input',
-    name = 'debug-inspect-global',
-    key_sequence = 'CONTROL + SHIFT + ENTER'
-  }
-}
